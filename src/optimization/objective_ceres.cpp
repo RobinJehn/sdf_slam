faf514593f3a65347c1d9c1eb59d8542d06f5124
RobinJehn <robin.jehn@gmx.de>
2024-10-11 19:17:05 +0100

    move code to cpp

--- src/optimization/objective_ceres.cpp
+++ src/optimization/objective_ceres.cpp
@@ -115,6 +115,37 @@ template <int Dim> int ObjectiveFunctorCeres<Dim>::num_outputs() const {
   return num_outputs_;
 }
 
+ManualCostFunction::ManualCostFunction(ObjectiveFunctorCeres<2> *functor)
+    : functor_(functor) {
+  set_num_residuals(functor->num_outputs());
+  mutable_parameter_block_sizes()->push_back(functor->num_inputs());
+}
+
+bool ManualCostFunction::Evaluate(double const *const *parameters,
+                                  double *residuals, double **jacobians) const {
+  Eigen::Map<const Eigen::VectorXd> params(parameters[0],
+                                           functor_->num_inputs());
+
+  // Compute residuals
+  Eigen::VectorXd res;
+  functor_->compute_residuals(params, res);
+  for (int i = 0; i < res.size(); ++i) {
+    residuals[i] = res[i];
+  }
+
+  if (jacobians != nullptr) {
+    Eigen::MatrixXd jac;
+    functor_->df(params, jac);
+    for (int i = 0; i < jac.rows(); ++i) {
+      for (int j = 0; j < jac.cols(); ++j) {
+        jacobians[0][i * functor_->num_inputs() + j] = jac(i, j);
+      }
+    }
+  }
+
+  return true;
+}
+
 // Explicit template instantiation
 template struct ObjectiveFunctorCeres<2>;
 template struct ObjectiveFunctorCeres<3>;
--- src/optimization/objective_ceres.hpp
+++ src/optimization/objective_ceres.hpp
@@ -40,35 +40,10 @@ private:
 
 class ManualCostFunction : public ceres::CostFunction {
 public:
-  ManualCostFunction(ObjectiveFunctorCeres<2> *functor) : functor_(functor) {
-    set_num_residuals(functor->num_outputs());
-    mutable_parameter_block_sizes()->push_back(functor->num_inputs());
-  }
+  ManualCostFunction(ObjectiveFunctorCeres<2> *functor);
 
   virtual bool Evaluate(double const *const *parameters, double *residuals,
-                        double **jacobians) const override {
-    Eigen::Map<const Eigen::VectorXd> params(parameters[0],
-                                             functor_->num_inputs());
-
-    // Compute residuals
-    Eigen::VectorXd res;
-    functor_->compute_residuals(params, res);
-    for (int i = 0; i < res.size(); ++i) {
-      residuals[i] = res[i];
-    }
-
-    if (jacobians != nullptr) {
-      Eigen::MatrixXd jac;
-      functor_->df(params, jac);
-      for (int i = 0; i < jac.rows(); ++i) {
-        for (int j = 0; j < jac.cols(); ++j) {
-          jacobians[0][i * functor_->num_inputs() + j] = jac(i, j);
-        }
-      }
-    }
-
-    return true;
-  }
+                        double **jacobians) const override;
 
 private:
   ObjectiveFunctorCeres<2> *functor_;
