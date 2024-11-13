a7879deffb4bc0e31f747394c2f1594bfed34335
RobinJehn <robin.jehn@gmx.de>
2024-11-13 22:43:04 +0000

    Make everything modular

---
+++ src/source_files.cmake
@@ -0,0 +1,25 @@
+# Paths to source files in subfolders
+set(MAP_SOURCES
+    map/map.cpp
+    map/utils.cpp
+)
+
+set(SCAN_SOURCES
+    scan/generate.cpp
+)
+
+set(STATE_SOURCES
+    state/state.cpp
+)
+
+set(OPTIMIZATION_SOURCES_COMMON
+    optimization/utils.cpp
+)
+
+set(VISUALIZE_SOURCES
+    visualize/utils.cpp
+)
+
+set(CONFIG_SOURCES
+    config/utils.cpp
+)
